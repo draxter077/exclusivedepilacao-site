3f36d4888abd0e7b385e0bf10feec08b3d95b723
philippe <philippe.idalgoprestes@gmail.com>
2025-09-02 12:11:04 -0300

    photos

--- pages/main/services/carroussel/scroller/main.js
+++ pages/main/services/carroussel/scroller/main.js
@@ -16,14 +16,10 @@ export default function scroller(){
         }`
 
     const scroller = cE("div", style)
-    scroller.appendChild(photo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1T_cLOzD5VLFYRZiqYDPHNTHSvS550hbs-Q&s"))
-    scroller.appendChild(photo("https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/blog/wordpress/prod/sites/7/2023/08/23134321/shutterstock_1825933745.jpg"))
-    scroller.appendChild(photo("https://www.coloramaesmaltes.com.br/-/media/Project/Loreal/Brand-Sites/Essie/MASTER/DMI/articles/tips_trends/Como-ser-uma-manicure-de-sucesso-Confira-dicas/manicure-de-sucesso.jpg?h=600&w=600&la=en&hash=87FFF505A9D06ABBE500C0DCC43916FB6B05D577"))
-    scroller.appendChild(photo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR104C9kQus6wPvE-_etxRXZKB5HFk3ikLLPQ&s"))
-    scroller.appendChild(photo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRkkJLr8Pg3hpzcnEyRqImV6YHJ7O6UZtBlXg&s"))
-    scroller.appendChild(photo("https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR1T_cLOzD5VLFYRZiqYDPHNTHSvS550hbs-Q&s"))
-    scroller.appendChild(photo("https://res.cloudinary.com/beleza-na-web/image/upload/f_auto,fl_progressive,q_auto:eco/blog/wordpress/prod/sites/7/2023/08/23134321/shutterstock_1825933745.jpg"))
-    scroller.appendChild(photo("https://www.coloramaesmaltes.com.br/-/media/Project/Loreal/Brand-Sites/Essie/MASTER/DMI/articles/tips_trends/Como-ser-uma-manicure-de-sucesso-Confira-dicas/manicure-de-sucesso.jpg?h=600&w=600&la=en&hash=87FFF505A9D06ABBE500C0DCC43916FB6B05D577"))
+    scroller.appendChild(photo("./assets/visual/cabelo.jpg"))
+    scroller.appendChild(photo("./assets/visual/depil.jpg"))
+    scroller.appendChild(photo("./assets/visual/rosto.jpg"))
+    scroller.appendChild(photo("./assets/visual/make.jpg"))
     
     window.addEventListener(
         "load",
--- pages/main/team/options/option/img/main.js
+++ pages/main/team/options/option/img/main.js
@@ -3,6 +3,7 @@ export default function img(i){
         {
             width:70%;
             border-radius:5px;
+            box-shadow:0px 0px 2px 0px var(--colorBlack);
         }`
 
     const img = cE("img", style)
